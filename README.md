9dba80119109cd953820c7ff2d4652612e7094fc
Sean Watson <seanwatson@MacBook-Pro.local>
2022-05-08 14:14:31 -0600

    initial commit, created html, readme, js and css

---
+++ README.md
@@ -0,0 +1,3 @@
+<!-- must include a screenshot of our wireframe plan...use trello.com
+include things that you want to do later, and things you're scrapping for later -->
+<!-- must have deployed link in the readme to go directly to the web app -->
---
+++ script.js
@@ -0,0 +1,15 @@
+// console.log($)
+
+const URL = "https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple"
+
+$.ajax(URL).then(function(data) {
+  console.log(data)
+})
+
+// VARIABLES
+
+// ELEMENT REFERENCES
+
+// EVENT LISTENERS
+
+// FUNCTIONS
