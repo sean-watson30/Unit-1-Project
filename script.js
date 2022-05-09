90655296a60844af305cad04a44dbe22415ceafd
Sean Watson <seanwatson@MacBook-Pro.local>
2022-05-09 09:22:16 -0600

    updated some to arrow functions for practice, created display Options function, need to populate

--- script.js
+++ script.js
@@ -23,34 +23,32 @@ function getRiddle() {
     const riddle = data.results[randomIndex].question
     // return riddle
     displayRiddle(riddle)
+    displayOptions(riddle)
   }, function(error) {
     console.log('something went wrong')
   })
 } 
 
-// const riddle = data[randomIndex]
-// console.log(riddle)
-
-function displayRiddle(riddle) {
 // connect the randomIndex riddle to the DOM in the <main> by appending to $mainContent
-  console.log(riddle)
+const displayRiddle = (riddle) => {
+  // console.log(riddle)
   $mainContent.empty();
 
   const $riddleHeader = $('<h2>')
-  $riddleHeader.text('The Sphynx speaks out to you in a supernatural voice. "Answer, if you dare!"')
+  $riddleHeader.text('The Sphynx speaks out to you in a supernatural voice: "Answer, if you dare!"')
   $mainContent.append($riddleHeader)
 
   const $riddleText = $('<p>')
+  $riddleText.text(riddle)
   $mainContent.append($riddleText)
 
 }
-
 // do I need a getOptions function also?
-
-function displayOptions() {
 // get the answer options form the API object and connect them to the answer buttons
   // maybe use DOM to create these buttons, so we can use .text to put the answer right in the buttons themselves (rather than hard-coding button A, B, etc)
+  
+const displayOptions = (riddle) => {
+    console.log(riddle)
 }
-
 // write some game logic (if/else, etc) as needed to figure out if the player selected the correct button
   // might be difficult, will maybe need to look at randomizing the button positions? (stretch goal, not necessarily MVP)
