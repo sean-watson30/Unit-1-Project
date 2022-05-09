650c3b9c78d5b94f7a39869d693c272962df0b9f
Sean Watson <seanwatson@MacBook-Pro.local>
2022-05-09 10:10:35 -0600

    removd unneeded comments, etc, and organized JS

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const $mainContent = $('main')
 $playBtn.on('click', getRiddle)
 
 // FUNCTIONS
-
+// getRiddle uses $.ajax to get info form APi and randomize the riddle question, and call displayRiddle() and displayOptions()
 function getRiddle() {
   const randomIndex = Math.floor(Math.random() * 10)
   // console.log(randomIndex)
@@ -22,14 +22,12 @@ function getRiddle() {
     // console.log(data)
     const riddle = data.results[randomIndex]
     // was const riddle = data.results[randomIndex].question...but as I'm referencing later to get answers as well, I thinkk it won't work here, so calling .question below in $riddleText
-    // return riddle
     displayRiddle(riddle)
     displayOptions(riddle)
   }, function(error) {
     console.log('something went wrong')
   })
 } 
-
 // connect the randomIndex riddle to the DOM in the <main> by appending to $mainContent
 const displayRiddle = (riddle) => {
   // console.log(riddle)
@@ -44,22 +42,26 @@ const displayRiddle = (riddle) => {
   $mainContent.append($riddleText)
 
 }
-// do I need a getOptions function also? (maybe not, if I pass riddle into disaplyOptions below)
-// get the answer options form the API object and connect them to the answer buttons
-  // maybe use DOM to create these buttons, so we can use .text to put the answer right in the buttons themselves (rather than hard-coding button A, B, etc)
-  
+// connect the answers/options as buttons to the DOM in the <main>
 const displayOptions = (riddle) => {
-  console.log(riddle)
-
+  // console.log(riddle)
   const $btnMakerA = $(`<button>${riddle.correct_answer}</button>`)
   $btnMakerA.addClass('correctAnswer')
   $mainContent.append($btnMakerA)
 
-  const $btnMakerW = $(`<button>${riddle.incorrect_answers}</button>`)
-  $btnMakerW.addClass('wrongAnswer')
-  $mainContent.append($btnMakerW)
+  const $btnMakerW1 = $(`<button>${riddle.incorrect_answers[0]}</button>`)
+  $btnMakerW1.addClass('wrongAnswer')
+  $mainContent.append($btnMakerW1)
 
+  const $btnMakerW2 = $(`<button>${riddle.incorrect_answers[1]}</button>`)
+  $btnMakerW2.addClass('wrongAnswer')
+  $mainContent.append($btnMakerW2)
 
+  const $btnMakerW3 = $(`<button>${riddle.incorrect_answers[2]}</button>`)
+  $btnMakerW3.addClass('wrongAnswer')
+  $mainContent.append($btnMakerW3)
 }
-// write some game logic (if/else, etc) as needed to figure out if the player selected the correct button
+
+
+// write some game logic (if/else, etc) as needed to figure out if the player selected the correct button...render() function needed here?
 // might be difficult, will maybe need to look at randomizing the button positions? (stretch goal, not necessarily MVP)...possibly adding them to a randomIndex sorta thing of an array?
