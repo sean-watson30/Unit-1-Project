8911c6b14a94cb092c6e75830809edd62065ae72
Sean Watson <seanwatson@MacBook-Pro.local>
2022-05-12 11:51:15 -0600

    added second 'medium' level API trivia questions/related functions

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // VARIABLES
 
-const URL = "https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple"
+const URLeasy = "https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple"
+const URLmedium = "https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple"
   
 // ELEMENT REFERENCES
 
 const $playBtn = $('#playBtn')
+const $playAgain = $('#playAgain')
 const $mainContent = $('main')
 const $headerContent = $('header')
 const $body = $('body')
@@ -12,15 +14,13 @@ const $div = $('div')
 
 // EVENT LISTENERS
 
-$playBtn.on('click', getRiddle)
+$playBtn.on('click', getRiddleEasy)
 
 // FUNCTIONS
 
-// getRiddle uses $.ajax to get info from APi and randomize the riddle question, and call displayRiddle() and displayOptions()
-function getRiddle() {
+function getRiddleEasy() {
   const randomIndex = Math.floor(Math.random() * 10)
-  // get array index from API data (I tried to use .length method, but couldn't figure out how, so I went with hard-coding *10 since the API only has 10 index options)
-  $.ajax(URL).then(function(data) {
+  $.ajax(URLeasy).then(function(data) {
     const riddle = data.results[randomIndex]
     displayRiddle(riddle)
     displayOptions(riddle)
@@ -28,37 +28,45 @@ function getRiddle() {
     console.log('something went wrong')
   })
 } 
+
+function getRiddleMedium() {
+  const randomIndex = Math.floor(Math.random() * 10)
+  $.ajax(URLmedium).then(function(data) {
+    const riddle = data.results[randomIndex]
+    console.log(riddle)
+    displayRiddle(riddle)
+    displayOptions(riddle)
+  }, function(error) {
+    console.log('something went wrong')
+  })
+}
 // connect the randomIndex riddle to the DOM in the <main> by appending to $mainContent
 const displayRiddle = (riddle) => {
   $mainContent.empty();
-
+  
   const $riddleHeader = $('<h2>')
   $riddleHeader.text('The Sphinx speaks out to you in a supernatural voice: "Answer the following, if you dare!"')
   $mainContent.append($riddleHeader)
-
+  
   const $riddleText = $('<p>')
   $riddleText.text(riddle.question)
   $mainContent.append($riddleText)
-
+  
 }
 // connect the answers/options as buttons to the DOM in the <main>
 const displayOptions = (riddle) => {
   const choices = []
   
   const $btnMakerA = $(`<button>${riddle.correct_answer}</button>`)
-  // $btnMakerA.addClass('correctAnswer')
   choices.push($btnMakerA)
   
   const $btnMakerW1 = $(`<button>${riddle.incorrect_answers[0]}</button>`)
-  $btnMakerW1.addClass('wrongAnswer')
   choices.push($btnMakerW1)
   
   const $btnMakerW2 = $(`<button>${riddle.incorrect_answers[1]}</button>`)
-  $btnMakerW2.addClass('wrongAnswer')
   choices.push($btnMakerW2)
   
   const $btnMakerW3 = $(`<button>${riddle.incorrect_answers[2]}</button>`)
-  $btnMakerW3.addClass('wrongAnswer')
   choices.push($btnMakerW3)
   
   const shuffleOptions = (choices) => {
@@ -66,7 +74,7 @@ const displayOptions = (riddle) => {
   }
   shuffleOptions(choices)
   $mainContent.append(choices)
-
+  
   $btnMakerA.on('click', playerChoiceCorrect)
   $btnMakerW1.on('click', playerChoiceWrong)
   $btnMakerW2.on('click', playerChoiceWrong)
@@ -76,9 +84,13 @@ const displayOptions = (riddle) => {
 // a function to compare player selection with answers
 const playerChoiceCorrect = () => {
   $mainContent.empty();
-
+  
   const win = $('<h1>Your answer is correct! The sphinx spares your life...this time.</h1>')
-  $mainContent.prepend(win)
+  $mainContent.append(win)
+  
+  const $playAgain = $('<button id="playAgain">Play Again?</button>')
+  $mainContent.append($playAgain);
+  $playAgain.on('click', getRiddleMedium)
 }
 const playerChoiceWrong = () => {
   $div.empty();
@@ -91,7 +103,7 @@ const playerChoiceWrong = () => {
   
   const lose = $('<h1 class="lostText">You have answered incorrectly. The sphinx will now take your life!!!!</h1>')
   $body.prepend(lose)
-
+  
   // const $startOver = $('<button id="playBtn">Play Again?</button>')
   // $body.prepend($startOver);
   // to later add a start over button to refresh page
